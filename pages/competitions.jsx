b9fcd48d2e49768e072429d8217998e606019951
Daniel Huang <dan@yellowiki.xyz>
2019-09-23 20:58:36 -0400

    Revert "Update competitions.jsx (#2)"
    This reverts commit 02acc67e8ccc984bb09d0e8cdbf7f39275617198.

--- pages/competitions.jsx
+++ pages/competitions.jsx
@@ -7,34 +7,6 @@ export default () => (
     <Container>
       <div style={{ textAlign: 'center' }}>
         <img
-          style={{ paddingTop: '10px' }}
-          alt="PCTF Logo"
-          src="https://ipfs.yellowiki.xyz/ipfs/QmXVRBeBQ8osBtL6i6oGYfuskYqovFhLvyUGKVkTTwK7kk/pctf.svg"
-        />
-      </div>
-
-      <p>
-        Online cybersecurity competition where teams hack various things to find
-        hidden &quotflag&quot
-      </p>
-      <p>
-        <b>Location:</b> Online/Carnegie Mellon University
-      </p>
-      <p>
-        <b>Date:</b> September 27, 2019
-      </p>
-      <p>
-        <b>Expenses:</b> $0
-      </p>
-      <p>
-        <b>Prizes:</b>
-      </p>
-      <p> - $14,000 in prizes</p>
-      <p> - Paid trip to Carnegie Mellon University</p>
-      <HorizontalLine />
-
-      <div style={{ textAlign: 'center' }}>
-        <img
           alt="CAC Logo"
           src="https://ipfs.yellowiki.xyz/ipfs/QmPNU4954i9PkEy2PkBnRCdXzwQ2zrqjoEDGzmCAWuGdY8/cac1.png"
         />
