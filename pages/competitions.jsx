c8450fc6edfb8f44817a3c1efdf5f0d43db53351
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-11-13 17:43:28 -0500

    Update competitions.jsx

--- pages/competitions.jsx
+++ pages/competitions.jsx
@@ -8,82 +8,25 @@ export default () => (
       <div style={{ textAlign: 'center' }}>
         <img
           style={{ paddingTop: '10px' }}
-          alt="PCTF Logo"
-          src="https://ipfs.yellowiki.xyz/ipfs/QmXVRBeBQ8osBtL6i6oGYfuskYqovFhLvyUGKVkTTwK7kk/pctf.svg"
+          alt="HSPC Logo  "
+          src="https://cdn.glitch.com/bd9736b1-da91-4d78-bdce-2e6bade0d565%2Fhspc.PNG?v=1573683617983"
         />
       </div>
 
       <p>
-        Online cybersecurity competition where teams hack various things to find
-        hidden &quot;flags&quot;
+      HSPC is a 3 hour competition where high school students from the DC metropolitan area compete in teams of four to demonstrate their programming skills and problem solving abilities.
       </p>
       <p>
-        <b>Location:</b> Online/Carnegie Mellon University
+        <b>Location:</b> CSIC at University of Maryland
       </p>
       <p>
-        <b>Date:</b> September 27, 2019
+        <b>Date:</b> February 23, 2020
       </p>
       <p>
-        <b>Expenses:</b> $0
+      <b>More information will follow!</b>
       </p>
-      <p>
-        <b>Prizes:</b>
-      </p>
-      <p> - $14,000 in prizes</p>
-      <p> - Paid trip to Carnegie Mellon University</p>
       <HorizontalLine />
-
-      <div style={{ textAlign: 'center' }}>
-        <img
-          alt="CAC Logo"
-          src="https://ipfs.yellowiki.xyz/ipfs/QmPNU4954i9PkEy2PkBnRCdXzwQ2zrqjoEDGzmCAWuGdY8/cac1.png"
-        />
-      </div>
-
-      <p>
-        Online competition where you make an app and create a video about it.
-      </p>
-      <p>
-        <b>Location:</b> Online/Capitol Hill
-      </p>
-      <p>
-        <b>Date:</b> November 1, 2019
-      </p>
-      <p>
-        <b>Expenses:</b> $0
-      </p>
-      <p>
-        <b>Prizes:</b>
-      </p>
-      <p> - Meet Congressman Elijah Cummings</p>
-      <p> - $500 in AWS Credits</p>
-      <p> - Free trip to Capitol Hill</p>
-
-      <HorizontalLine />
-
-      <div style={{ textAlign: 'center' }}>
-        <img
-          alt="LHD Logo"
-          src="https://ipfs.yellowiki.xyz/ipfs/QmPNU4954i9PkEy2PkBnRCdXzwQ2zrqjoEDGzmCAWuGdY8/lhd.png"
-        />
-      </div>
-
-      <p>
-        Local hackathon where you build software and showcase it in one day.
-      </p>
-      <p>
-        <b>Location:</b> University Of Maryland
-      </p>
-      <p>
-        <b>Date:</b> December 2019
-      </p>
-      <p>
-        <b>Expenses:</b> Hackers must provide their own transportation to/from
-      </p>
-      <p>
-        <b>Prizes:</b>
-      </p>
-      <p> - Not released yet</p>
+      <p>More competitions will follow!</p>
     </Container>
   </Layout>
 )
